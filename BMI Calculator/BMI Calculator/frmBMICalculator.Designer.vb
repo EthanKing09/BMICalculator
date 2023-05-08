7b32b963dc6202e7cbfd289aa915c3a4c4289b72
Ethan King <131774253+EthanKing09@users.noreply.github.com>
2023-05-08 08:05:42 -0400

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/frmBMICalculator.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBMICalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.picBMI = New System.Windows.Forms.PictureBox()
+        Me.lstSystemSelect = New System.Windows.Forms.ListBox()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.lblWeight = New System.Windows.Forms.Label()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.picBMI, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Transparent
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(117, 16)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(192, 31)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "BMI Calculator"
+        '
+        'picBMI
+        '
+        Me.picBMI.Image = Global.BMI_Calculator.My.Resources.Resources.BMIImage
+        Me.picBMI.Location = New System.Drawing.Point(38, 345)
+        Me.picBMI.Name = "picBMI"
+        Me.picBMI.Size = New System.Drawing.Size(351, 213)
+        Me.picBMI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBMI.TabIndex = 1
+        Me.picBMI.TabStop = False
+        '
+        'lstSystemSelect
+        '
+        Me.lstSystemSelect.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstSystemSelect.FormattingEnabled = True
+        Me.lstSystemSelect.ItemHeight = 24
+        Me.lstSystemSelect.Items.AddRange(New Object() {"Imperial", "Metric"})
+        Me.lstSystemSelect.Location = New System.Drawing.Point(166, 66)
+        Me.lstSystemSelect.Name = "lstSystemSelect"
+        Me.lstSystemSelect.Size = New System.Drawing.Size(95, 52)
+        Me.lstSystemSelect.TabIndex = 2
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.BackColor = System.Drawing.Color.Transparent
+        Me.lblHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeight.ForeColor = System.Drawing.Color.White
+        Me.lblHeight.Location = New System.Drawing.Point(102, 148)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(75, 24)
+        Me.lblHeight.TabIndex = 3
+        Me.lblHeight.Text = "Height: "
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.BackColor = System.Drawing.Color.Transparent
+        Me.lblWeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeight.ForeColor = System.Drawing.Color.White
+        Me.lblWeight.Location = New System.Drawing.Point(102, 189)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(79, 24)
+        Me.lblWeight.TabIndex = 4
+        Me.lblWeight.Text = "Weight: "
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Enabled = False
+        Me.txtHeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHeight.Location = New System.Drawing.Point(218, 145)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 29)
+        Me.txtHeight.TabIndex = 5
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Enabled = False
+        Me.txtWeight.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWeight.Location = New System.Drawing.Point(218, 186)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(100, 29)
+        Me.txtWeight.TabIndex = 6
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.BackColor = System.Drawing.Color.Transparent
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.White
+        Me.lblResult.Location = New System.Drawing.Point(162, 295)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(99, 24)
+        Me.lblResult.TabIndex = 7
+        Me.lblResult.Text = "BMI: XX.X"
+        Me.lblResult.Visible = False
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Enabled = False
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(106, 237)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(103, 39)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Enabled = False
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(215, 237)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(103, 39)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmBMICalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.BMI_Calculator.My.Resources.Resources.Background
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(426, 565)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtWeight)
+        Me.Controls.Add(Me.txtHeight)
+        Me.Controls.Add(Me.lblWeight)
+        Me.Controls.Add(Me.lblHeight)
+        Me.Controls.Add(Me.lstSystemSelect)
+        Me.Controls.Add(Me.picBMI)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "frmBMICalculator"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "BMI Calculator"
+        CType(Me.picBMI, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents picBMI As PictureBox
+    Friend WithEvents lstSystemSelect As ListBox
+    Friend WithEvents lblHeight As Label
+    Friend WithEvents lblWeight As Label
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ BMI Calculator/BMI Calculator/frmBMICalculator.vb
@@ -0,0 +1,133 @@
+﻿'   Program Name:   BMI Calculator
+'   Purpose:        Calculate user's BMI based on height and weight
+'   Developer:      Ethan King
+'   Date:           4/25/2023
+Public Class frmBMICalculator
+    Private Sub frmBMICalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Threading.Thread.Sleep(1000)
+    End Sub
+
+    Private Sub lstSystemSelect_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstSystemSelect.SelectedIndexChanged
+        Dim intSystemSelect As Integer
+
+        intSystemSelect = lstSystemSelect.SelectedIndex
+        Select Case intSystemSelect
+            Case 0
+                lblHeight.Text = "Height (in)"
+                lblWeight.Text = "Weight (lb)"
+            Case 1
+                lblHeight.Text = "Height (m)"
+                lblWeight.Text = "Weight (kg)"
+        End Select
+
+        '   Enable text boxes and buttons
+        txtHeight.Enabled = True
+        txtWeight.Enabled = True
+        btnCalculate.Enabled = True
+        btnClear.Enabled = True
+    End Sub
+
+    Private Function fncValidateHeight() As Boolean
+        '   Declare variables
+        Dim decHeight As Integer
+        Dim blnHeightValidityCheck As Boolean = False
+        Dim strHeightMessage As String = "Please enter your height, above 0."
+        Dim strMessageTitle As String = "Error"
+        '   Test for errors
+        Try
+            decHeight = Convert.ToDecimal(txtHeight.Text)
+            If decHeight >= 1 Then
+                blnHeightValidityCheck = True
+
+            Else
+                MsgBox(strHeightMessage, , strMessageTitle)
+                txtHeight.Focus()
+                txtHeight.Clear()
+            End If
+
+
+        Catch ex As FormatException
+            MsgBox(strHeightMessage, , strMessageTitle)
+            txtHeight.Focus()
+            txtHeight.Clear()
+        Catch ex As OverflowException
+            MsgBox(strHeightMessage, , strMessageTitle)
+            txtHeight.Focus()
+            txtHeight.Clear()
+        Catch ex As SystemException
+            MsgBox(strHeightMessage, , strMessageTitle)
+            txtHeight.Focus()
+            txtHeight.Clear()
+        End Try
+
+        Return blnHeightValidityCheck
+    End Function
+
+    Private Function fncValidateWeight() As Boolean
+        '   Declare variables
+        Dim decWeight As Decimal
+        Dim blnWeightValidityCheck As Boolean = False
+        Dim strWeightMessage As String = "Please enter your weight"
+        Dim strMessageTitle As String = "Error"
+
+        '   Test for errors
+        Try
+            decWeight = Convert.ToDecimal(txtWeight.Text)
+            blnWeightValidityCheck = True
+
+        Catch ex As FormatException
+            MsgBox(strWeightMessage, , strMessageTitle)
+            txtWeight.Clear()
+        Catch ex As OverflowException
+            MsgBox(strWeightMessage, , strMessageTitle)
+            txtWeight.Clear()
+        Catch ex As SystemException
+            MsgBox(strWeightMessage, , strMessageTitle)
+            txtWeight.Clear()
+        End Try
+
+        Return blnWeightValidityCheck
+    End Function
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        '   Declare Variables
+        Dim decHeight As Decimal
+        Dim decWeight As Decimal
+        Dim blnHeightValid As Boolean = False
+        Dim blnWeightValid As Boolean = False
+        Dim decFinalBMI As Decimal
+        Dim intSystemSelect As Integer
+
+        intSystemSelect = lstSystemSelect.SelectedIndex
+        '   Are height and weight valid?
+        blnHeightValid = fncValidateHeight()
+        blnWeightValid = fncValidateWeight()
+
+        If (blnHeightValid And blnWeightValid) Then
+            decHeight = Convert.ToDecimal(txtHeight.Text)
+            decWeight = Convert.ToDecimal(txtWeight.Text)
+
+            '   Select which formula to use to calculate BMI
+            Select Case intSystemSelect
+                Case 0
+                    '   Calculate BMI
+                    decFinalBMI = ((decWeight / (decHeight * decHeight)) * 703)
+                Case 1
+                    '   Calculate BMI
+                    decFinalBMI = (decWeight / (decHeight * decHeight))
+            End Select
+            '   Display BMI
+            lblResult.Text = "BMI: " & decFinalBMI.ToString("N1")
+            lblResult.Visible = True
+
+
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        '   Clear out data
+        txtHeight.Clear()
+        txtWeight.Clear()
+        lblResult.Text = ""
+
+    End Sub
+End Class
